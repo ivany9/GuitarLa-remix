9aabe608cdc338fe369fa452911ba713e2fb220b
Jorge Ivan Rodriguez <ivany9@gmail.com>
2024-01-14 06:05:03 +1100

    loader tienda

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,19 +1,59 @@
+import  nosotros from '../../public/img/nosotros.jpg'
+import styles from '../Styles/nosotros.css'
+
 export function meta(){
  return[
 {
- title:'GuitarLa-Nosotros'
+ title:'GuitarLa-Nosotros',
+ description:' shop on line'
+
+}
+
+] 
 
 
 }
+export function links()
+{
+return [
+  {
+  rel:'stylesheet',
+  href: styles
+  },
+  {
+    rel:'preload' ,
+    href:'nosotros',
+    as:'image' 
+  
+  }
 
 ]
 
 
 }
 
+
+
+
 function Nosotros() {
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+        <div className="contenido">
+          <img 
+          src={nosotros}
+          alt="imagen nosotros"
+          />
+          <div>
+             <p> Proin et ante non quam egestas rhoncus non vitae ante. Pellentesque posuere tincidunt velit, eget congue massa dapibus euismod. Sed sed imperdiet augue. Quisque nunc magna, posuere eu lacus vitae, malesuada maximus nisi. Proin cursus bibendum odio, non viverra massa tincidunt rutrum. Sed ut tempus urna. Mauris lobortis volutpat fringilla. Donec a tempus nunc, eu blandit ligula.</p>
+              
+             <p> Proin et ante non quam egestas rhoncus non vitae ante. Pellentesque posuere tincidunt velit, eget congue massa dapibus euismod. Sed sed imperdiet augue. Quisque nunc magna, posuere eu lacus vitae, malesuada maximus nisi. Proin cursus bibendum odio, non viverra massa tincidunt rutrum. Sed ut tempus urna. Mauris lobortis volutpat fringilla. Donec a tempus nunc, eu blandit ligula.</p>
+      
+          </div>
+          
+          
+          </div>  
+    </main>
   )
 }
 
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+
+import {useLoaderData } from '@remix-run/react';
+import {getGuitarras} from '../../app/models/guitarras.server';
+
+export async function loader(){
+//   const respuesta=await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+//   const resultado= await respuesta.json()
+   
+// return{}
+
+//segunda forma de hacer el fecth 
+ const guitarras = await getGuitarras()
+
+return guitarras
+}
 
 function Tienda() {
+const guitarras=useLoaderData()
+console.log(guitarras)
+
   return (
     <div>tienda</div>
   )
